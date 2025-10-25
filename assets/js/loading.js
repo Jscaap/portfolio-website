24f26def7b01af95de91947731a56447993e6213
Jessica Aprilia Putri <jessicaputri2005@icloud.com>
2025-10-25 09:12:18 +0800

    chore: initial commit with existing portfolio files

---
+++ assets/js/loading.js
@@ -0,0 +1,9 @@
+// window.addEventListener("load", function () {
+//     const preloader = document.getElementById("preloader");
+//     preloader.style.opacity = "0";
+//     preloader.style.pointerEvents = "none";
+//     setTimeout(() => {
+//       preloader.style.display = "none";
+//     }, 500); // Fade out delay
+// });
+
